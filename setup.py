c55e2ae4fa211734a6e05eb20ddcb2f1c7f78c62
Are Jensen <arejensen@gmail.com>
2019-02-01 12:31:09 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='orgid_validator',
+      version='0.0.1',
+      description='Validates Norwegian orgid as issued by Brønnøysundregistrene',
+      url='https://github.com/arejensen/orgid_validator',
+      author='Are Jensen',
+      author_email='arejensen@gmail.com',
+      license='GNU General Public License version 3',
+      packages=['orgid_validator'])
